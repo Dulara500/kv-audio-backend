f1f50999c2b9bec88401e8dd6d5f0fcfa8ab6ecf
Dulara500 <madushankadulara38@gmail.com>
2025-10-21 12:05:23 +0530

    removed whatsapp attribute from user model 🤬

--- models/User.js
+++ models/User.js
@@ -31,11 +31,8 @@ const userSchema = new mongoose.Schema({
     phone:{
         type : String,
         required : true
-    },
-    whatsapp:{
-        type : String,
-        required : true
     }
+    
 })
 const User = mongoose.model("User",userSchema);
 export default User;
